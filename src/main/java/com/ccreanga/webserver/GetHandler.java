46d03096e89274aaf3a6589f9377a29116b7871f
cornel creanga <cornelcreanga@yahoo.com>
2016-08-01 23:48:33 +0300

    started basic support for conditionals added todo's

--- src/main/java/com/ccreanga/webserver/GetHandler.java
+++ src/main/java/com/ccreanga/webserver/GetHandler.java
@@ -20,6 +20,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.Date;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -29,7 +30,7 @@ import static com.ccreanga.webserver.formatters.DateUtil.FORMATTER_RFC822;
 
 public class GetHandler {
 
-    public void handleGetResponse(RequestMessage request, boolean hasBody, Configuration configuration, OutputStream out) throws IOException {
+    public void handleGetResponse(RequestMessage request, boolean writeBody, Configuration configuration, OutputStream out) throws IOException {
 
         HTTPHeaders responseHeaders = new HTTPHeaders();
         HTTPStatus responseStatus;
@@ -113,34 +114,128 @@ public class GetHandler {
 //                return new ResponseMessage(HTTPStatus.PRECONDITION_FAILED);
 //        }
 
-
-        //Everything is ok, we will build the headers
-        //todo - fix me
-        if (!hasBody) {
-            writeNoBodyResponse(responseHeaders, HTTPStatus.OK, out);
-            return;
-        }
-
-        String indexPage = null;
-
+        //todo - not ok, it can crash (eg internal server error)
         writeResponseLine(HTTPStatus.OK, out);
 
 
         if (file.isFile()) {
-            deliverFile(request,responseHeaders,file,configuration,out);
+            deliverFile(request,responseHeaders,file,configuration,writeBody,out);
         } else {
-            deliverFolder(request,responseHeaders,file,configuration,out);
+            deliverFolder(request,responseHeaders,file,configuration,writeBody,out);
         }
     }
 
-    private void deliverFile(RequestMessage request,HTTPHeaders responseHeaders,File file,Configuration configuration,OutputStream out) throws IOException{
-        responseHeaders.putHeader(HTTPHeaders.CONTENT_TYPE, Mime.getType(Files.getFileExtension((file.getName()))));
-        responseHeaders.putHeader(HTTPHeaders.LAST_MODIFIED, DateUtil.formatDate(Instant.ofEpochMilli(file.lastModified()),DateUtil.FORMATTER_RFC822));
+    private HTTPStatus evaluateConditional(RequestMessage request,HTTPHeaders responseHeaders,String etag){
+        //https://tools.ietf.org/html/rfc7232#section-6
+        HTTPStatus response = HTTPStatus.OK;
+        String ifMatch = request.getHeader(HTTPHeaders.IF_MATCH);
+        String IfUnmodifiedSince =  request.getHeader(HTTPHeaders.IF_UNMODIFIED_SINCE);
+        String ifNoneMatch  =  request.getHeader(HTTPHeaders.IF_NONE_MATCH);
+        String ifModifiedSince  =  request.getHeader(HTTPHeaders.IF_MODIFIED_SINCE);
+
+        return response;
+//        String header = headers.get("If-Match");
+//        if (header != null && !match(true, splitElements(header, false), etag))
+//            return 412;
+//        // If-Unmodified-Since
+//        Date date = headers.getDate("If-Unmodified-Since");
+//        if (date != null && lastModified > date.getTime())
+//            return 412;
+//        // If-Modified-Since
+//        int status = 200;
+//        boolean force = false;
+//        date = headers.getDate("If-Modified-Since");
+//        if (date != null && date.getTime() <= System.currentTimeMillis()) {
+//            if (lastModified > date.getTime())
+//                force = true;
+//            else
+//                status = 304;
+//        }
+//        // If-None-Match
+//        header = headers.get("If-None-Match");
+//        if (header != null) {
+//            if (match(false, splitElements(header, false), etag)) // RFC7232#3.2: use weak matching
+//                status = req.getMethod().equals("GET")
+//                        || req.getMethod().equals("HEAD") ? 304 : 412;
+//            else
+//                force = true;
+//        }
+//        return force ? 200 : status;
+
+//        if (ifMatch != null) {
+//            if (!etag.equals(ifMatch)){
+//                return HTTPStatus.PRECONDITION_FAILED;
+//            }
+//            if (ifNoneMatch!=null){
+//                (!etag.equals(ifMatch)){
+//            }
+//
+//        }
+//        if (etag.equals(value))
+//        if (value != null) {
+//            String etag = EtagManager.getDateBasedEtag(file);//weak etag for the moment
+//            if (etag.equals(value))
+//                return new ResponseMessage(HTTPStatus.PRECONDITION_FAILED);
+//        }
+    }
+
+    private void deliverFile(RequestMessage request,HTTPHeaders responseHeaders,File file,Configuration configuration,boolean writeBody,OutputStream out) throws IOException{
+
         String etag = null;
-        if (configuration.getRequestEtag().equals(Configuration.ETAG_WEAK)) {
+        String mime = Mime.getType(Files.getFileExtension((file.getName())));
+
+        responseHeaders.putHeader(HTTPHeaders.CONTENT_TYPE,mime);
+        responseHeaders.putHeader(HTTPHeaders.LAST_MODIFIED, DateUtil.formatDate(Instant.ofEpochMilli(file.lastModified()),DateUtil.FORMATTER_RFC822));
+
+        if (configuration.getRequestEtag().equals(Configuration.ETAG_WEAK) && request.getVersion().equals(HTTPVersion.HTTP_1_1)) {
             etag = EtagManager.getInstance().getFileEtag(file, true);
             responseHeaders.putHeader(HTTPHeaders.ETAG, etag);
+            HTTPStatus statusAfterConditionals = evaluateConditional(request,responseHeaders,etag);
+            if (!statusAfterConditionals.equals(HTTPStatus.OK)){
+
+            }
         }
+
+
+        if (request.getVersion().equals(HTTPVersion.HTTP_1_1)){
+            responseHeaders.putHeader(HTTPHeaders.TRANSFER_ENCODING,"chunked");
+
+            if (request.headerContains(HTTPHeaders.ACCEPT_ENCODING,"gzip") && shouldCompress(mime)){
+                responseHeaders.putHeader(HTTPHeaders.CONTENT_ENCODING,"gzip");
+            }else if (request.headerContains(HTTPHeaders.ACCEPT_ENCODING,"deflate")  && shouldCompress(mime)){
+                responseHeaders.putHeader(HTTPHeaders.CONTENT_ENCODING,"deflate");
+            }
+
+            writeHeaders(responseHeaders, out);
+
+            OutputStream enclosed = new ChunkedOutputStream(out);
+            if (request.headerContains(HTTPHeaders.ACCEPT_ENCODING,"gzip") && shouldCompress(mime)){
+                enclosed = new GZIPOutputStream(enclosed);
+            }else if (request.headerContains(HTTPHeaders.ACCEPT_ENCODING,"deflate") && shouldCompress(mime)){
+                enclosed = new DeflaterOutputStream(enclosed);
+            }
+            if (writeBody) {
+                ByteStreams.copy(new FileInputStream(file), enclosed);
+            }
+            enclosed.close();//this will not close the response stream - the chunkedoutputstream does not propagate the close method
+        }else {
+            //http 1.0 does not support chunk => we will not support gzip - it will be too expensive
+            String length = String.valueOf(file.length());
+            ContextHolder.get().setContentLength(length);
+            responseHeaders.putHeader(HTTPHeaders.CONTENT_LENGTH, writeBody?length:"0");
+            writeHeaders(responseHeaders, out);
+            if (writeBody) {
+                ByteStreams.copy(new FileInputStream(file), out);
+            }
+        }
+
+    }
+
+    private void deliverFolder(RequestMessage request,HTTPHeaders responseHeaders,File file,Configuration configuration,boolean writeBody,OutputStream out) throws IOException{
+        //todo - it should not return html unless the client accepts that
+
+        responseHeaders.putHeader(HTTPHeaders.CONTENT_TYPE, Mime.getType("html"));
+        String indexPage = TemplateRepository.instance().buildIndex(file, configuration.getServerRootFolder());
 
         if (request.getVersion().equals(HTTPVersion.HTTP_1_1)){
             responseHeaders.putHeader(HTTPHeaders.TRANSFER_ENCODING,"chunked");
@@ -153,35 +248,37 @@ public class GetHandler {
 
             writeHeaders(responseHeaders, out);
 
-            ChunkedOutputStream chunkedOutputStream = new ChunkedOutputStream(out);
-            OutputStream enclosed = chunkedOutputStream;
-            if (request.headerContains(HTTPHeaders.ACCEPT_ENCODING,"gzip")){
+            OutputStream enclosed = new ChunkedOutputStream(out);
+            if (request.headerContains(HTTPHeaders.ACCEPT_ENCODING,"gzip") ){
                 enclosed = new GZIPOutputStream(enclosed);
-            }else if (request.headerContains(HTTPHeaders.ACCEPT_ENCODING,"deflate")){
+            }else if (request.headerContains(HTTPHeaders.ACCEPT_ENCODING,"deflate") ){
                 enclosed = new DeflaterOutputStream(enclosed);
             }
-
-            ByteStreams.copy(new FileInputStream(file), enclosed);
-            enclosed.close();//this will not close the response stream - the chunkedoutputstream does not propagate the close method
-        }else {
-            //http 1.0 does not support chunk => we will not support gzip too - it will be too expensive
-            String length = String.valueOf(file.length());
-            ContextHolder.get().setContentLength(length);
-            responseHeaders.putHeader(HTTPHeaders.CONTENT_LENGTH, length);
+            if (writeBody) {
+                enclosed.write(indexPage.getBytes(Charsets.UTF_8));
+            }
+            enclosed.close();
+        }else{
+            ContextHolder.get().setContentLength("" + indexPage.length());
+            responseHeaders.putHeader(HTTPHeaders.CONTENT_LENGTH, writeBody?"" + indexPage.length():"0");
             writeHeaders(responseHeaders, out);
-            ByteStreams.copy(new FileInputStream(file), out);
+            if (writeBody) {
+                out.write(indexPage.getBytes(Charsets.UTF_8));
+            }
         }
 
     }
 
-    private void deliverFolder(RequestMessage request,HTTPHeaders responseHeaders,File file,Configuration configuration,OutputStream out) throws IOException{
-        //todo - it should not return html unless the client accepts that
-        String indexPage = TemplateRepository.instance().buildIndex(file, configuration.getServerRootFolder());
-        responseHeaders.putHeader(HTTPHeaders.CONTENT_TYPE, Mime.getType("html"));
-        ContextHolder.get().setContentLength("" + indexPage.length());
-        responseHeaders.putHeader(HTTPHeaders.CONTENT_LENGTH, "" + indexPage.length());
-        writeHeaders(responseHeaders, out);
-        out.write(indexPage.getBytes(Charsets.UTF_8));
+    private boolean shouldCompress(String mimetype) {
+        //todo - refine that
+        if (mimetype.startsWith("text") ||
+                mimetype.contains("xml") ||
+                mimetype.contains("json") ||
+                mimetype.contains("javascript") ||
+                mimetype.contains("html")
+                )
+            return true;
+        return false;
 
     }
 
